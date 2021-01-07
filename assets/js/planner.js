cd6eed62e0d1cfde94d8164569d030bfe96b9272
mdu830 <mdu830@yahoo.com>
2021-01-07 12:48:36 -0500

    organized planner js

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -11,51 +11,59 @@ $(document).ready(function(){
     var $date = $("#dateSpan");
     $date.text(currentDateDay);
 
-    var storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
+    var storedEvents = JSON.parse(localStorage.getItem("storedEvents"));
   
     // If plans were retrieved from localStorage, update the plan array to it
-    if (storedPlans !== null) {
-      planTextArr = storedPlans;
+    if (storedEvents !== null) {
+
+      inputArr = storedEvents;
     } else {
-      planTextArr = new Array(12);
+
+      inputArr = new Array(12);
     }
 
-    var $plannerContainer = $('#plannerContainer');
-    $plannerContainer.empty();
+    var $planContainer = $('#planContainer');
+
+    $planContainer.empty();
 
     for (var hr = 6; hr <= 17; hr++) {
 
         index = hr - 6;
         
         // rows
-        var $rowDiv = $('<div>');
-        $rowDiv.addClass('row');
-        $rowDiv.addClass('plannerRow');
-        $rowDiv.attr('hr-index',hr);
+        var $row = $('<div>');
+        $row.addClass('row');
+        $row.addClass('plannerRow');
+        $row.attr('hr-index',hr);
       
         //col and timeDiv
-        var $col2TimeDiv = $('<div>');
-        $col2TimeDiv.addClass('col-2');
+        var $timeDiv = $('<div>');
+        $timeDiv.addClass('col-2');
+
         var $timeSpan = $('<span>');
         $timeSpan.attr('class','timeDiv');
         
         // AM or PM time of day
         var displayHour = 0;
         var midDay = "";
+
         if (hr > 12) { 
+
           displayHour = hr - 12;
           midDay = "pm";
+
         } else {
+
           displayHour = hr;
           midDay = "am";
+
         }
         
         // put time in timeDiv
         $timeSpan.text(`${displayHour} ${midDay}`);
     
-    
-        $rowDiv.append($col2TimeDiv);
-        $col2TimeDiv.append($timeSpan);
+        $row.append($timeDiv);
+        $timeDiv.append($timeSpan);
         
         //input column for event 
         var $dailyPlanSpn = $('<input>');
@@ -65,27 +73,28 @@ $(document).ready(function(){
         $dailyPlanSpn.attr('type','text');
         $dailyPlanSpn.attr('class','dailyPlan');
     
-        $dailyPlanSpn.val( planTextArr[index] );
+        $dailyPlanSpn.val( inputArr[index] );
         
-        var $colInputDiv = $('<div>');
-        $colInputDiv.addClass('col');
+        var $userInputSection = $('<div>');
 
-        $rowDiv.append($colInputDiv);
-        $colInputDiv.append($dailyPlanSpn);
+        $userInputSection.addClass('col');
+
+        $row.append($userInputSection);
+        $userInputSection.append($dailyPlanSpn);
     
         // save button/div
-        var $saveDiv = $('<i>');
-        $saveDiv.addClass('col-1 btn btn-primary btn-block');
-        $saveDiv.attr('id',`saveid-${index}`);
-        $saveDiv.attr('save-id',index);
-        $saveDiv.attr("alt", "Save Event");
-        $saveDiv.attr("title", "Save Event")
+        var $saveBtn = $('<saveIt>');
+        $saveBtn.addClass('col-1 btn btn-primary btn-block');
+        $saveBtn.attr('id',`saveid-${index}`);
+        $saveBtn.attr('save-id',index);
+        $saveBtn.attr("alt", "Save Event");
+        $saveBtn.attr("title", "Save Event")
         
         var $imgDiv = $('<img>');
         $imgDiv.attr('src', "assets/images/saveicon.png");
 
-        $rowDiv.append($saveDiv);
-        $saveDiv.append($imgDiv);
+        $row.append($saveBtn);
+        $saveBtn.append($imgDiv);
     
         //change row color according to past present or future time
         function updateColor ($hourRow,hr) { 
@@ -95,10 +104,13 @@ $(document).ready(function(){
               $hourRow.css("background-color","lightgrey");
               $hourRow.css("opacity", "70%");
               $hourRow.css("-webkit-opacity", "0.7");
+
             } else if ( hr > now) {
+
               $hourRow.css("background-color","lightgreen");
               $hourRow.css("opacity", "100%");
               $hourRow.css("-webkit-opacity", "0.8");
+
             } else {
               $hourRow.css("background-color","tomato");
               $hourRow.css("opacity", "95%");
@@ -106,21 +118,22 @@ $(document).ready(function(){
             }
           };
 
-        updateColor($rowDiv, hr);
-        $plannerContainer.append($rowDiv);
+        updateColor($row, hr);
+        $planContainer.append($row);
       };
 
-      $(document).on('click','i', function(event) {
+      $(document).on('click','saveIt', function(event) {
         event.preventDefault();  
 
         $index = $(this).attr('save-id');
     
-        var inputId = '#input-'+ $index;
-        var $value = $(inputId).val();
-    
-        planTextArr[$index] = $value;
+        var id = '#input-'+ $index;
 
-        localStorage.setItem("storedPlans", JSON.stringify(planTextArr));
+        var $value = $(id).val();
+    
+        inputArr[$index] = $value;
+
+        localStorage.setItem("storedEvents", JSON.stringify(inputArr));
       }); 
 
 });
